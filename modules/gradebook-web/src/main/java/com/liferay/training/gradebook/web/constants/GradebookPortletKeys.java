f7a1d874ed7f439f01842d86c75fed555b1c4495
Monika Domiter <domitermonika@gmail.com>
2023-05-09 12:01:01 +0200

    3 - set the portlet name

--- modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
+++ modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -1,11 +1,11 @@
 package com.liferay.training.gradebook.web.constants;
 
 /**
- * @author user
+ * @author Monika Domiter
  */
 public class GradebookPortletKeys {
 
 	public static final String GRADEBOOK =
-		"com_liferay_training_gradebook_web_GradebookPortlet";
+		"com_liferay_training_gradebook_web_portlet_GradebookPortlet";
 
 }
