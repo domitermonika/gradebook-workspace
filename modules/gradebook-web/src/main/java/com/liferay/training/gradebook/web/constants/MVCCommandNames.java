92d96388eb19e39cbc90c881040ea5f81176ad66
Monika Domiter <domitermonika@gmail.com>
2023-05-09 12:53:16 +0200

    3 - constant class to hold command names

---
+++ modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/MVCCommandNames.java
@@ -0,0 +1,32 @@
+package com.liferay.training.gradebook.web.constants;
+
+/**
+ * @author Monika Domiter
+ */
+public class MVCCommandNames {
+
+	public static final String ADD_ASSIGNMENT = "/gradebook/assignment/add";
+
+	public static final String ADD_SUBMISSION = "/gradebook/submission/add";
+
+	public static final String DELETE_ASSIGNMENT =
+		"/gradebook/assignment/delete";
+
+	public static final String DELETE_SUBMISSION =
+		"/gradebook/submission/delete";
+
+	public static final String EDIT_ASSIGNMENT = "/gradebook/assignment/edit";
+
+	// These are used for the optional exercise "Implement Submissions".
+
+	public static final String EDIT_SUBMISSION = "/gradebook/submission/edit";
+
+	public static final String GRADE_SUBMISSION = "/gradebook/submission/grade";
+
+	public static final String VIEW_ASSIGNMENT = "/gradebook/assignment/view";
+
+	public static final String VIEW_ASSIGNMENTS = "/gradebook/assignments/view";
+
+	public static final String VIEW_SUBMISSION = "/gradebook/submission/view";
+
+}
