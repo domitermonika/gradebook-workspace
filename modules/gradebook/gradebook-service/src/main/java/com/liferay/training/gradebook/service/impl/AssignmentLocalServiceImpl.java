5c41866694de6040116280b2369b5eca4c82df99
Monika Domiter <domitermonika@gmail.com>
2023-05-09 11:05:03 +0200

    2 - implemented finder methods

--- modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentLocalServiceImpl.java
+++ modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/service/impl/AssignmentLocalServiceImpl.java
@@ -15,14 +15,20 @@
 package com.liferay.training.gradebook.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.dao.orm.Disjunction;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.training.gradebook.model.Assignment;
 import com.liferay.training.gradebook.service.base.AssignmentLocalServiceBaseImpl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -85,10 +91,42 @@ public class AssignmentLocalServiceImpl extends AssignmentLocalServiceBaseImpl {
 		return super.addAssignment(assignment);
 	}
 
+	public List<Assignment> getAssignmentByGroupId(long groupId) {
+		return assignmentPersistence.findByGroupId(groupId);
+	}
+
+	public List<Assignment> getAssignmentByGroupId(
+		long groupId, int start, int end) {
+
+		return assignmentPersistence.findByGroupId(groupId, start, end);
+	}
+
+	public List<Assignment> getAssignmentByGroupId(
+		long groupId, int start, int end,
+		OrderByComparator<Assignment> orderByComparator) {
+
+		return assignmentPersistence.findByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	public List<Assignment> getAssignmentsByKeywords(
+		long groupId, String keywords, int start, int end,
+		OrderByComparator<Assignment> orderByComparator) {
+
+		return assignmentLocalService.dynamicQuery(
+			getKeywordSearchDynamicQuery(groupId, keywords), start, end,
+			orderByComparator);
+	}
+
+	public long getAssignmentsCountByKeywords(long groupId, String keywords) {
+		return assignmentLocalService.dynamicQueryCount(
+			getKeywordSearchDynamicQuery(groupId, keywords));
+	}
+
 	public Assignment updateAssignment(
 			long assignmentId, String title, String description, Date dueDate,
 			ServiceContext serviceContext)
-			throws PortalException {
+		throws PortalException {
 
 		// Get the Assignment by id
 
@@ -103,6 +141,27 @@ public class AssignmentLocalServiceImpl extends AssignmentLocalServiceBaseImpl {
 		assignment = super.updateAssignment(assignment);
 
 		return assignment;
+	}
+
+	private DynamicQuery getKeywordSearchDynamicQuery(
+		long groupId, String keywords) {
+
+		DynamicQuery dynamicQuery = dynamicQuery().add(
+			RestrictionsFactoryUtil.eq("groupId", groupId));
+
+		if (Validator.isNotNull(keywords)) {
+			Disjunction disjunctionQuery =
+				RestrictionsFactoryUtil.disjunction();
+
+			disjunctionQuery.add(
+				RestrictionsFactoryUtil.like("title", "%" + keywords + "%"));
+			disjunctionQuery.add(
+				RestrictionsFactoryUtil.like(
+					"description", "%" + keywords + "%"));
+			dynamicQuery.add(disjunctionQuery);
+		}
+
+		return dynamicQuery;
 	}
 
 }
