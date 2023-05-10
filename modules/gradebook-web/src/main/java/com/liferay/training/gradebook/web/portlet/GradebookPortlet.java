20fbfe80ac422225456d833ea527d8e1267165ca
Monika Domiter <domitermonika@gmail.com>
2023-05-10 14:46:08 +0200

    4 - added portlet Component  - css

--- modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
+++ modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -13,6 +13,7 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	property = {
 		"com.liferay.portlet.display-category=category.training",
+		"com.liferay.portlet.css-class-wrapper=gradebook-portlet",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=false",
 		"javax.portlet.display-name=Gradebook",
